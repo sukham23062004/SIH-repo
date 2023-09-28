e7cb4425ae5b4e6b7ac8e470a2418c301b86cb59
Sukham Singh <124015833+sukham23062004@users.noreply.github.com>
2023-09-29 01:42:34 +0530

    Add files via upload

---
+++ src/app/login/page.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import {FcGoogle} from 'react-icons/fc'
+
+const login = () => {
+  return (
+    <body className="max-h-screen">
+      <section className="border-red-500 bg-gray-200 min-h-screen flex items-center justify-center">
+        <div className="bg-gray-100 p-5 flex rounded-2xl shadow-lg max-w-3xl">
+          <div className="md:w-1/2 px-5">
+            <h2 className="text-2xl font-bold text-[#002D74]">Login</h2>
+            <p className="text-sm mt-4 text-[#002D74]">
+              If you have an account, please login
+            </p>
+            <form className="mt-6" action="#" method="POST">
+              <div>
+                <label className="block text-gray-700">Email Address</label>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                  placeholder="Enter Email Address"
+                  className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none text-slate-950"
+                  required
+                />
+              </div>
+
+              <div className="mt-4">
+                <label className="block text-gray-700">Password</label>
+                <input
+                  type="password"
+                  name=""
+                  id=""
+                  placeholder="Enter Password"
+                  className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                  focus:bg-white focus:outline-none text-slate-950"
+                  required
+                />
+              </div>
+
+              <div className="text-right mt-2">
+                <a
+                  href="#"
+                  className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6"
+              >
+                Log In
+              </button>
+            </form>
+
+            <div className="mt-7 grid grid-cols-3 items-center text-gray-500">
+              <hr className="border-gray-500" />
+              <p className="text-center text-sm">OR</p>
+              <hr className="border-gray-500" />
+            </div>
+
+            <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 ">
+              <FcGoogle></FcGoogle>
+              <span className="ml-4 text-slate-950">Login with Google</span>
+            </button>
+
+            <div className="text-slate-950 text-sm flex justify-between items-center mt-3">
+              <p>If you dont have an account...</p>
+              <a className="py-2 px-5 ml-3 bg-white border rounded-xl hover:scale-110 duration-300 border-blue-400 font-bold text-[#002D74]" href='signup'>
+                Sign Up
+              </a>
+            </div>
+          </div>
+          <div className="w-1/2 md:block hidden ">
+            <img
+              src="https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+              className="rounded-2xl"
+              alt="page img"
+            />
+          </div>
+        </div>
+      </section>
+    </body>
+  );
+}
+
+export default login
---
+++ src/app/page.tsx
@@ -0,0 +1,342 @@
+import { DiYeoman } from "react-icons/di";
+import { AiOutlineCloudUpload, AiOutlineCloudDownload } from "react-icons/ai";
+import { BsInstagram, BsFacebook, BsGithub, BsTwitter } from "react-icons/bs";
+import login from "./login/page";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col bg-white">
+      <header className="text-white body-font bg-black">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-around">
+          <a
+            className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+            href="#"
+          >
+            <DiYeoman className="w-8 h-8"></DiYeoman>
+            <span className="ml-3 text-xl">Language Converter</span>
+          </a>
+          <div>
+            <a
+              className="inline-flex items-center bg-slate-800 border-0 py-2 px-6 focus:outline-none hover:bg-slate-600 rounded-lg text-base mt-4 md:mt-0"
+              href="login"
+            >
+              Login
+            </a>
+            <a
+              className="inline-flex items-center bg-slate-800 border-0 py-2 px-6 focus:outline-none hover:bg-slate-600 rounded-lg text-base mt-4 md:mt-0 ml-5"
+              href="signup"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <section className="text-slate-900 body-font">
+        <div className="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-extrabold text-gray-900">
+              Before they sold out
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+              park mlkshk tote bag selvage hot chicken authentic tumeric
+              truffaut hexagon try-hard chambray.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+                <label className="flex flex-row">
+                  <div className="flex items-center mr-2">
+                    <AiOutlineCloudUpload></AiOutlineCloudUpload>
+                  </div>
+                  <span>Upload</span>
+                  <input type="file" className="hidden" />
+                </label>
+              </button>
+              <button className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg ml-5">
+                <label className="flex flex-row">
+                  <div className="flex items-center mr-2">
+                    <AiOutlineCloudDownload></AiOutlineCloudDownload>
+                  </div>
+                  <span>Download</span>
+                  <input type="file" className="hidden" />
+                </label>
+              </button>
+            </div>
+            <div>{/* DropDown Menu */}</div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://dummyimage.com/720x600"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="text-slate-900 body-font">
+        <div className="container px-5 py-18 mx-auto flex flex-wrap">
+          <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">
+              1
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-extrabold title-font text-gray-900 mb-1 text-xl">
+                  Shooting Stars
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">
+              2
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-extrabold title-font text-gray-900 mb-1 text-xl">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">
+              3
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-extrabold title-font text-gray-900 mb-1 text-xl">
+                  The 400 Blows
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+            <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">
+              4
+            </div>
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-extrabold title-font text-gray-900 mb-1 text-xl">
+                  Neptune
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-slate-900 body-font">
+        <div className="container px-5 py-20 mx-auto">
+          <div className="flex flex-col text-center w-full mb-10">
+            <h1 className="text-4xl font-extrabold title-font mb-4 text-gray-900 capitalize">
+              History
+            </h1>
+          </div>
+          <div className="flex flex-wrap -m-4 justify-center">
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/200x200"
+                />
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/201x201"
+                />
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/202x202"
+                />
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/4 md:w-1/2">
+              <div className="h-full flex flex-col items-center text-center">
+                <img
+                  alt="team"
+                  className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                  src="https://dummyimage.com/203x203"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="text-slate-900 body-font">
+        <div className="container px-5 pb-8 mx-auto">
+          <div className="flex flex-col text-center w-full mb-16">
+            <h1 className="sm:text-3xl text-4xl font-extrabold capitalize title-font mb-4 text-gray-900">
+              Progress
+            </h1>
+          </div>
+          <div className="flex flex-wrap -m-4 text-center">
+            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="text-purple-500 w-12 h-12 mb-3 inline-block"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8 17l4 4 4-4m-4-5v9"></path>
+                  <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+                </svg>
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  2.7K
+                </h2>
+                <p className="leading-relaxed">Downloads</p>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="text-purple-500 w-12 h-12 mb-3 inline-block"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+                </svg>
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  1.3K
+                </h2>
+                <p className="leading-relaxed">Users</p>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="text-purple-500 w-12 h-12 mb-3 inline-block"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M3 18v-6a9 9 0 0118 0v6"></path>
+                  <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+                </svg>
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  74
+                </h2>
+                <p className="leading-relaxed">Files</p>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+              <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  className="text-purple-500 w-12 h-12 mb-3 inline-block"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                </svg>
+                <h2 className="title-font font-medium text-3xl text-gray-900">
+                  46
+                </h2>
+                <p className="leading-relaxed">Places</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="text-slate-900 body-font">
+        <div className="container px-5 py-4 pt-12 mx-auto flex items-center sm:flex-row flex-col">
+          <a
+            className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0"
+            href="#"
+          >
+            <DiYeoman className="w-8 h-8"></DiYeoman>
+            <span className="ml-3 text-xl">Language Converter</span>
+          </a>
+          <p className="text-sm sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-slate-900 sm:py-2 sm:mt-0 mt-4 text-slate-900">
+            © 2023 Devify_7174
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a
+              className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0 mr-2"
+              href="#"
+            >
+              <BsInstagram className="w-8 h-8 text-slate-900"></BsInstagram>
+            </a>
+            <a
+              className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0 mr-2"
+              href="#"
+            >
+              <BsFacebook className="w-8 h-8"></BsFacebook>
+            </a>
+            <a
+              className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0 mr-2"
+              href="#"
+            >
+              <BsGithub className="w-8 h-8"></BsGithub>
+            </a>
+            <a
+              className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0 mr-2"
+              href="#"
+            >
+              <BsTwitter className="w-8 h-8"></BsTwitter>
+            </a>
+          </span>
+        </div>
+      </footer>
+    </div>
+  );
+}
